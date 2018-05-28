07295e4ab5532e4185ed20b07d2445844818ec08
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-05-28 22:45:22 +1000

    Added method to play a frequency

--- sound.c
+++ sound.c
@@ -1,3 +1,25 @@
+/*
+ * sound.c
+ *
+ * Created: 5/27/2018 6:18:13 PM
+ *  Author: Xinyi Li
+ */ 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 8000000UL	// 8MHz
+#include <util/delay.h>
+#include <stdio.h>
+
+#include "timer0.h"
+#include "project.h"
+
+int global_disable_sound = 0;
+int sound_on = 0;
+int sound_quiet = 0;
+int playing_sound = 0;
+uint32_t current_time;
+uint32_t last_play_time;
+
 uint16_t freq_to_clock_period(uint16_t freq) {
 	return (1000000UL / freq);	// UL makes the constant an unsigned long (32 bits)
 	// and ensures we do 32 bit arithmetic, not 16
@@ -26,4 +48,35 @@ void init_sound(void) {
 	DDRD &= ~(1 << PIND3) | ~(1 << PIND5);
 	PCICR |= (1 << PCIE3);
 	PCMSK3 = (1 << PCINT27) | (1 << PCINT29);
+}
+
+uint16_t clockperiod;
+uint16_t pulsewidth;
+float dutycycle = 2;
+void play_sound(uint16_t freq, uint32_t duration) {
+	sound_on = bit_is_set(PIND, PIND3) == 8 ? 1 : 0;
+	sound_quiet = bit_is_set(PIND, PIND5) == 32 ? 1 : 0;
+	if (!paused) {
+		if (sound_quiet && sound_on)
+			pulsewidth = duty_cycle_to_pulse_width(0.15, clockperiod);
+		else if (sound_on)
+			pulsewidth = duty_cycle_to_pulse_width(dutycycle, clockperiod);
+		else
+			pulsewidth = duty_cycle_to_pulse_width(0, clockperiod);
+	}
+	clockperiod = freq_to_clock_period(freq);
+
+
+	// Set the maximum count value for timer/counter 1 to be one less than the clockperiod
+	OCR1A = clockperiod - 1;
+	TCCR1A = (1 <<WGM11) | (1 << WGM10);
+	TCCR1B = (1 << WGM13) | (1 << WGM12) | (0 << CS12) | (1 << CS11) | (0 << CS10);
+	TIMSK1 |= (1 << OCIE1A);
+	TIFR1 &= (1 << OCF1A);
+	
+	enable_sound();
+
+	current_time = get_current_time();
+	last_play_time = get_current_time() + duration;
+	
 }
