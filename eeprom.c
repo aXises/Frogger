5f1990146dc314c2d0e60d7eb32895086efcbc19
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-05-31 02:12:51 +1000

    Set up EEPROM block write

--- eeprom.c
+++ eeprom.c
@@ -12,3 +12,20 @@
 #include "terminalio.h"
 #include "project.h"
 
+uint32_t offset = 50;
+uint32_t offset_s = 150;
+
+void write_eeprom_name(uint8_t name[12], uint8_t index) {
+	while(EECR & (1<<EEPE));
+	int8_t interrupts_on = bit_is_set(SREG, SREG_I);
+	cli();
+	
+	EECR |= (1<<EEMPE);
+	EECR |= (1<<EEPE);
+	
+	eeprom_update_block((void*) name, (void*) (index * 12) + offset, 12);
+	
+	if (interrupts_on) {
+		sei();
+	}
+}
