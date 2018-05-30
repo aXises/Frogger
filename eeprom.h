345e37273e98a9a416096fe91593c894c8376616
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-05-31 02:13:37 +1000

    Added eeprom.h

---
+++ eeprom.h
@@ -0,0 +1,11 @@
+/*
+ * eeprom.h
+ *
+ * Created: 5/30/2018 4:58:10 PM
+ *  Author: Xinyi Li
+ */ 
+
+
+void write_eeprom(uint8_t name[12], uint32_t score, uint8_t index);
+void read_eeprom(void);
+
